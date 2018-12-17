d747c0cb287cbea3510812794c8042c7dd06450f
Lasse Legaard <legaard.lasse@gmail.com>
2018-12-17 19:38:15 +0100

    Updated number and string generator plus added fixture test

--- src/generators/__test__/string-generator.test.ts
+++ src/generators/__test__/string-generator.test.ts
@@ -8,6 +8,19 @@ describe('String Generator', () => {
         // Act
         const randomValues = Array(10).fill(undefined).map(() => sut.generate());
 
+        // Assert
         expect(randomValues.every(v => (randomValues.filter(f => f === v).length) === 1)).toBeTruthy();
+    });
+
+    test('should generate random string with prefix', () => {
+        // Arrange
+        const prefix = 'prefix';
+        const sut = new StringGenerator(prefix);
+
+        // Act
+        const randomValues = Array(10).fill(undefined).map(() => sut.generate());
+
+        // Assert
+        expect(randomValues.every(v => v.includes(prefix))).toBeTruthy();
     });
 });
--- src/generators/number-generator.ts
+++ src/generators/number-generator.ts
@@ -1,23 +1,27 @@
 import { ValueGenerator } from './value-generator';
 
 export class NumberGenerator implements ValueGenerator<number> {
-    private _min: number;
-    private _max: number;
+    private readonly _min: number;
+    private readonly _max: number;
 
     constructor(min: number, max: number) {
         this._min = min;
         this._max = max;
 
-        if (this._min < 0 || this._max < 1) {
-            throw new Error('Minimum value must be larger than 0 and maximum larger than 1');
+        if (this._min < 0) {
+            throw new Error('Minimum value cannot be smaller than 0');
+        }
+
+        if (this._max < 1) {
+            throw new Error('Maximum value cannot be smaller than 1');
         }
 
         if (this._min >= this._max) {
-            throw new Error('Minimum value must be smaller than maximum value');
+            throw new Error('Maximum value must be larger than minimum value');
         }
     }
 
     generate(): number {
-        return Math.floor((Math.random() * this._max) + this._min);
+        return Math.floor((Math.random() * (this._max + 1)) + this._min);
     }
 }
